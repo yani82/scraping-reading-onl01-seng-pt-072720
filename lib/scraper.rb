eecf5b2393a796cff96546399032af0a1b8a615e
Yani Asari <yani.iman1@gmail.com>
2020-09-01 06:52:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
-puts doc.css(".headline-26OIBN")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 require_relative './course.rb'
 
