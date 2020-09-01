0a8647529128516a674d65e24340807cd5de82a0
Yani Asari <yani.iman1@gmail.com>
2020-09-01 06:53:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,10 +5,11 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-
-require_relative './course.rb'
-
-class Scraper
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ 
+courses.each do |course|
+  puts course.text.strip
+end
   
   
   
