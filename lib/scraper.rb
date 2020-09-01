186fb83af5bf37739fa96e820902730be6f80b29
Yani Asari <yani.iman1@gmail.com>
2020-09-01 06:55:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ doc.css(".headline-26OIBN").text
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
  
 courses.each do |course|
   puts course.text.strip
